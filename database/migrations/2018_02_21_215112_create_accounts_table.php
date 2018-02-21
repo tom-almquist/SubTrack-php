2853be42a25736ce74c71dbf127f24cb281e72b6
tom-almquist <thomas.c.almquist@gmail.com>
2018-02-21 16:00:09 -0600

    Created migrations, edited .env to work with a database called crmtest

---
+++ database/migrations/2018_02_21_215112_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('service_id')->default(0);
+            $table->state('state')->default('confirmed');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
