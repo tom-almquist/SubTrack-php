b22db96b81ed626f41b33031db12c1356aa999fa
tom-almquist <thomas.c.almquist@gmail.com>
2018-02-21 16:07:32 -0600

    added confirm method

--- app/Account.php
+++ app/Account.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public static function confirm($first_name, $last_name, $email)
+        {
+            static::create([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email
+            ]);
+        }
 }
